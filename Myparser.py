e7f2b0e114c737c84de08ad4d2359d6c7c0c0352
renisa <renisawidya@protonmail.com>
2020-09-11 09:08:55 +0700

    add rough parser for tempo and cnn, still ugly, like you

--- Myparser.py
+++ Myparser.py
@@ -15,8 +15,30 @@ class Antara(HTMLParser):
         if (tag == 'script'):
             self.active = False
 
-class Tempo:
-    pass
+class Tempo(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.active = False
+    def handle_starttag(self, tag, attrs):
+        if (tag == 'p'):
+            self.active = True
+    def handle_data(self, text):
+        if self.active:
+            print(text)
+    def handle_endtag(self, tag):
+        if (tag == 'p'):
+            self.active = False
 
-class Cnn:
-    pass
+class Cnn(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.active = False
+    def handle_starttag(self, tag, attrs):
+        if (tag == 'p'):
+            self.active = True
+    def handle_data(self, text):
+        if self.active:
+            print(text)
+    def handle_endtag(self, tag):
+        if (tag == 'p'):
+            self.active = False
